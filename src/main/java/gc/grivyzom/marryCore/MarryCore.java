d7dcb4f8b00e07f3f878f0fecb23078ae42beef4
Brocolitx <grivyzom@gmail.com>
2025-07-06 01:58:41 -0400

    feat: Archivos base

---
+++ src/main/java/gc/grivyzom/marryCore/MarryCore.java
@@ -0,0 +1,17 @@
+package gc.grivyzom.marryCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MarryCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
